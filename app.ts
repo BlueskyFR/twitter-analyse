0be444c713b4caf671c41a0a4fb18d4b8344827c
Hugo Cartigny (BlueskyFR) <hugo.cartigny@gmail.com>
2020-10-02 01:23:43 +0200

    Add Luxon npm module and create getCurrentCourse()

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import dbg from "debug";
 import axios from "axios";
 //import { Parser, Calendar } from "ikalendar";
 import { async as ical, CalendarComponent } from "node-ical";
+import { DateTime, Settings } from "luxon";
 
 const config = require("./config.json5");
 
@@ -19,10 +20,30 @@ async function getCalendar(): Promise<CalendarComponent[]> {
   return Object.values(await ical.parseICS(await getICS()));
 }
 
+function getCurrentCourse(calendar: CalendarComponent[]): CalendarComponent | false {
+  const now = DateTime.local();
+
+  return (
+    calendar.find(
+      (event) =>
+        DateTime.fromJSDate(event.start as Date) <= now &&
+        DateTime.fromJSDate(event.end as Date) >= now
+    ) || false
+  );
+}
+
+/*
+DateTime.fromJSDate(event.start as Date) <= now &&
+DateTime.fromJSDate(event.end as Date) >= now
+*/
+
 async function main() {
+  // Set the defaut timezone to FR
+  Settings.defaultLocale = "fr";
+
   let calendar: CalendarComponent[] = await getCalendar();
 
-  debug(calendar);
+  debug(getCurrentCourse(calendar));
 }
 
 main(); // ISO 8601
